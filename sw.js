4c1ae7e3c3fbbe97e1041dce62b9bfb31d09ac69
Apple <apple@Apples-MacBook-Air-2.local>
2026-07-24 10:59:06 +0530

    Deploy 0.1% Genius Clean Build

---
+++ sw.js
@@ -0,0 +1,15 @@
+// Clean Service Worker Unregister & Invalidation
+self.addEventListener('install', () => {
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(cacheNames.map((cacheName) => caches.delete(cacheName)));
+    }).then(() => {
+      return self.clients.claim();
+    })
+  );
+  self.registration.unregister();
+});
