eac2a2c1039221b8dcab6bb6181c6651782dfe8d
Apple <apple@Apples-MacBook-Air-2.local>
2026-09-23 04:24:43 +0530

    Deploy 0.1% Genius Build - 2026-09-22T22:54:43.323Z

---
+++ assets/locationRoute.Cip0TmYA.js
@@ -0,0 +1,1 @@
+var e=e=>e.normalize(`NFKD`).replace(/[\u0300-\u036f]/g,``).toLowerCase().replace(/&/g,`and`).replace(/[^a-z0-9]+/g,`-`).replace(/^-+|-+$/g,``),t=t=>t.countrySlug||e(t.country),n=e=>`${t(e)}/${e.stateSlug}/${e.citySlug}`,r=e=>`/locations/${n(e)}/`,i=e=>`/locations/${t(e)}/${e.stateSlug}/`,a=(e,t,r)=>`/${e}/${t}/${n(r)}/`;export{a as n,i as r,r as t};
